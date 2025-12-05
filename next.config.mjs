7071f016ea10026f0c4e82b5163504dcc5f5ded1
aleming87 <aleming87@users.noreply.github.com>
2025-12-05 19:00:42 +0000

    Add redirect from /terms to /terms-and-conditions

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/terms',
+        destination: '/terms-and-conditions',
+        permanent: true, // 301 redirect
+      },
+    ]
+  },
+}
 
 export default nextConfig
